890db835ae6e0972a57023c2484ea1f8ed890a0a
Rodrigo <rodrigo19962010@live.com>
2017-06-26 19:50:27 -0300

    .

--- Runner.c
+++ Runner.c
@@ -4,9 +4,15 @@
 
 int main(){
 
+	while(1==1){
+
 	system("backup perform --trigger gerenciadorSala_development");
 
 	system("git add . && git add --all && git commit -am. && git push -u rodrigo master");
+
+	sleep(1000);
+
+	}
 
 	return 0;
 }
